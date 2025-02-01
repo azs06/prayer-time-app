5857a6e6f678a241869e99b297f11ae87baa527d
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-02-02 00:00:43 +0600

    tailwind added

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+	let { children } = $props();
+	import '../css/app.css';
+</script>
+
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<div class="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
+    <p class="text-white">tailwind added</p>
+    <p class="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
+      The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+    </p>
+  </div>

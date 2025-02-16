006d62073e49935d69dca0ed4e3bc00822e24141
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-02-16 18:31:27 +0600

    minor design fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,8 +12,8 @@
 
 <div class="min-h-full">
 	<header class=" shadow-sm">
-		<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-			<h1 class="text-3xl font-bold tracking-tight text-gray-900">
+		<div class="mx-auto px-4 py-6">
+			<h1 class="text-3xl font-bold text-center tracking-tight text-gray-900">
 				নামাজের সময়সূচী
 				{new Date().toLocaleDateString('bn').slice(-4)} (হিজরি
 				{arabicYear()})

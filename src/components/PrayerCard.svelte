45e83d7cdaabf80cda65c896de8c737bf18e2b98
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-02-16 18:02:26 +0600

    added translations

--- src/components/PrayerCard.svelte
+++ src/components/PrayerCard.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Clock, Sun, Sunrise, Sunset, Moon } from 'lucide-svelte';
+	import lang from "../lang/lang.json"
 	const defaultPrayerTimes = {
 		date: '',
 		sehri: '',
@@ -15,13 +16,13 @@
 
 	const timeSlots = $derived.by(() => {
 		return [
-			{ name: 'Sehri', time: prayerTime.sehri, icon: Moon },
-			{ name: 'Fazr', time: prayerTime.fazr, icon: Sunrise },
-			{ name: 'Sunrise', time: prayerTime.sunrise, icon: Sun },
-			{ name: 'Juhoor', time: prayerTime.juhoor, icon: Sun },
-			{ name: 'Asr', time: prayerTime.asr, icon: Sun },
-			{ name: 'Magrib/Iftar', time: prayerTime.magrib_iftar, icon: Sunset },
-			{ name: 'Isha', time: prayerTime.isha, icon: Moon }
+			{ name: lang.prayerTime.sehri.bn, time: prayerTime.sehri, icon: Moon },
+			{ name: lang.prayerTime.fazr.bn, time: prayerTime.fazr, icon: Sunrise },
+			{ name: lang.prayerTime.sunrise.bn, time: prayerTime.sunrise, icon: Sun },
+			{ name: lang.prayerTime.juhoor.bn, time: prayerTime.juhoor, icon: Sun },
+			{ name: lang.prayerTime.asr.bn, time: prayerTime.asr, icon: Sun },
+			{ name: lang.prayerTime.magrib_iftar.bn, time: prayerTime.magrib_iftar, icon: Sunset },
+			{ name: lang.prayerTime.isha.bn, time: prayerTime.isha, icon: Moon }
 		];
 	});
 </script>
@@ -29,7 +30,7 @@
 <div class="bg-white rounded-xl shadow-lg overflow-hidden">
 	<div class="bg-indigo-600 px-6 py-4">
 		<div class="flex items-center gap-2 text-white font-bold">
-			<Clock className="w-6 h-6" /> Today's Prayer Schedule
+			<Clock className="w-6 h-6" /> {lang.cardHeading.bn}
 		</div>
 	</div>
 	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-6">
